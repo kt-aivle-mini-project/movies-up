2b1d6f35ed937cb55333eda64a0fb9e79222d245
jeongwoo <92703457+songjeongwoo@users.noreply.github.com>
2022-01-27 09:33:54 +0900

    Delete miniproject directory

--- miniproject/registration/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class UserTable(models.Model):
-    user_id = models.CharField(primary_key=True, max_length=20)               
-    password = models.CharField(max_length=20)                                
-    email = models.CharField(null=True, max_length=50)
-    user_name = models.CharField(max_length=20)
