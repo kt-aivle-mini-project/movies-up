9d56cf0b0c99d47739a00e49be5c7450f827f94a
Heejun Kim <73517978+heejun32@users.noreply.github.com>
2022-01-27 09:34:18 +0900

    Delete miniproejct1 directory

--- miniproejct1/registration/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views
-
-app_name = 'registration'
-urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='singup'),
-]
