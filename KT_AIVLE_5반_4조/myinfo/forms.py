49bfbd205c459493ed3d6e9c989c14e956fd65de
Minhee Cho <aezjk12@ewhain.net>
2022-01-28 13:49:18 +0900

    Delete KT_AIVLE_5반_4조 directory

--- KT_AIVLE_5반_4조/myinfo/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import UserTable
-class SignUpForm(forms.ModelForm):
-    class Meta:
-        model = UserTable
-
-        fields = ['user_id', 'password', 'user_name']
